5e15c2cb6ff9632b216df854422728cd4795459b
Jason Knoll <22356211+jasonknoll@users.noreply.github.com>
2024-12-03 09:39:00 -0500

    Update main.rs

--- C8/src/main.rs
+++ C8/src/main.rs
@@ -6,6 +6,25 @@ fn main() {
 
 struct Chip {}
 
-struct ChipContext {}
+struct ChipContext {
+    memory: Vec<u8>,
+    V: Vec<u8>,
+    stack: Vec<u16>,
+    index: u16,
+    PC: u16,
+    SP: u16,
+    todo!("Add sound and delay timers")
+}
+
+impl ChipContext {
+    pub fn new() -> Self {
+        todo!("Init all member vars")
+    }
+
+    pub fn push() {}
+    pub fn pop() {}
+
+    pub fn loadIntoMemory() {}
+}
 
 struct ChipDisplay {}
